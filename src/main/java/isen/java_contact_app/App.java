736e786fb541123b623e21b8411140c8dd25516e
digif <digifi@laposte.net>
2019-02-04 09:56:36 +0100

    generating maven project files

---
+++ src/main/java/isen/java_contact_app/App.java
@@ -0,0 +1,13 @@
+package isen.java_contact_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
